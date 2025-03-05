5e7da65236c82d68b38d7f9fcb7db76c89793d66
Yoneibi Neira <yoneibi_10@gmail.com>
2025-03-04 23:07:26 -0500

    se crean componentes

---
+++ evaluacion-uno/src/components/Cards.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <div className="card">
+        <img src="https://via.placeholder.com/150" alt="Imagen" />
+        <h2>Titulo</h2>
+        <p>Descripción</p>
+        </div>
+    );
+};
+export default Card;
