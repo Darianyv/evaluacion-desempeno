061232e2210ce1738297930b6089f16bef05866d
Yoneibi Neira <yoneibi_10@gmail.com>
2025-03-06 23:38:50 -0500

    se corrije archivo de entrada

--- evaluacion-uno/src/main.jsx
+++ evaluacion-uno/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css'
+import './index.css'
 import Card from './components/Cards'
 
 createRoot(document.getElementById('root')).render(
